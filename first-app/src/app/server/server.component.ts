765ac81b8e24e6d798073e8d36b1a2d99644be90
Shaul <shaulyakar@gmail.com>
2018-06-25 07:46:10 +0300

    finished basic data binding (lecture 31)

--- first-app/src/app/server/server.component.ts
+++ first-app/src/app/server/server.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-server',
-  templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  selector: "app-server",
+  templateUrl: "./server.component.html",
+  styleUrls: ["./server.component.css"]
 })
 export class ServerComponent implements OnInit {
+  serverId: number = 10;
+  serverStatus: string = "Offline";
 
-  constructor() { }
-
-  ngOnInit() {
+  getServerStatus() {
+    return this.serverStatus;
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  selector: "app-servers",
+  templateUrl: "./servers.component.html",
+  styleUrls: ["./servers.component.css"]
 })
 export class ServersComponent implements OnInit {
+  allowAddServer = false;
+  serverCreationStatus = "Server Not Created Yet";
+  serverName = "Server Default Name";
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    setTimeout(() => {
+      this.allowAddServer = true;
+    }, 2000);
   }
 
+  ngOnInit() {}
+
+  onClick() {
+    this.serverCreationStatus = "Server " + this.serverName + " Was Created";
+  }
 }
