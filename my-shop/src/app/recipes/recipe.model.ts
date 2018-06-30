a41e0f2ba3ce50c77e1484b2c10c3efeccb816b0
Shaul <shaulyakar@gmail.com>
2018-06-30 20:04:48 +0300

    finished section 3 (lecture 55)

---
+++ my-shop/src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+import { ENAMETOOLONG } from "constants";
+
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, description: string, imagePath: string) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
--- my-shop/src/app/recipes/recipes-list/recipes-list.component.ts
+++ my-shop/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipes-list',
-  templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  selector: "app-recipes-list",
+  templateUrl: "./recipes-list.component.html",
+  styleUrls: ["./recipes-list.component.css"]
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Test Recipe",
+      "My Description",
+      "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg"
+    ),
+    new Recipe(
+      "Test Recipe 2",
+      "My Description 2",
+      "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
